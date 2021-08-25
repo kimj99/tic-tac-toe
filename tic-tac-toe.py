d2cb3e9be8af63f87dff68cefbfa01085e4491eb
kimj99 <jimankim@pugetsound.edu>
2021-08-24 17:05:43 -0700

    input invalid input logic

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -9,7 +9,10 @@ class Board:
         
     def get_spot(self, coordinate):
         x,y = coordinate
-        return self._board[x][y]
+        if x in range(len(self._board)) or y in range(len(self._board)):
+            return self._board[x][y]
+        else:
+            return 0
     
     def is_valid_move(self, move):
         if move == '*':
@@ -20,7 +23,6 @@ class Board:
         spot = self.get_spot(move)
         if self.is_valid_move(spot):
             self._board[move[0]][move[1]] = letter
-            self.draw()
             return True
         return False
 
@@ -34,6 +36,11 @@ class Player:
             board.draw()
             spot = input(f"Pick a coordinate: ").split(" ")
             spot = (int(spot[0]),int(spot[1]))
+            if board.get_spot(spot) == 0:
+                if not board.is_valid_move(board.get_spot(spot)):
+                    print("Invalid Move!")
+                    print("Try Again")
+                    continue
             return spot
 class Game:
     def __init__(self, p1: Player = Player('X'), p2: Player = Player('O'),board:Board = Board()) -> None:
@@ -47,12 +54,14 @@ class Game:
             if self.p1_turn:
                 move = self.p1.move(self.board)
                 self.board.make_move(move, self.p1.letter)
+                self.p1_turn = False
             else:
                 move = self.p2.move(self.board)
                 self.board.make_move(move, self.p2.letter)
-
+                self.p1_turn = True
 def main():
-    pass
+    game = Game()
+    game.start()
 
 
 main()
